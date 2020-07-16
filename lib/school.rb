232ac28e1be25219bd52229c551f27a0576bb9bc
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-16 18:33:00 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+  def initialize(n)
+    @name = n
+    @roster = {}
+  end
+  attr_reader :roster
+  def add_student(a,b)
+    @roster[b] ||=[]
+    @roster[b] << a
+  end
+  def grade (g)
+    @roster[g]
+  end
+  def sort
+    @roster.each do |a,b|
+      @roster[a]= b.sort
+    end
+  end
+end
